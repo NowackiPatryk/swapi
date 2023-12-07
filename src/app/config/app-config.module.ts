de6c393ad5aeec1caa0126646c99097a218bb0dc
Patryk Nowacki prv <patryknowackiprv@MacBook-Air-Patryk.local>
2023-12-07 18:11:19 +0100

    exporting app config from app-config-module

--- src/app/config/app-config.module.ts
+++ src/app/config/app-config.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { AppConfig } from "./app-config";
 
 @Module({
-  providers: [AppConfig]
+  providers: [AppConfig],
+  exports: [AppConfig],
 })
 export class AppConfigModule {}
