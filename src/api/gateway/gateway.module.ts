6092ff6289c6e4fca765b708adedef867fc9ec32
Patryk Nowacki prv <patryknowackiprv@MacBook-Air-Patryk.local>
2023-12-10 17:24:25 +0100

    refactor: removed old example resolvers/tests

--- src/api/gateway/gateway.module.ts
+++ src/api/gateway/gateway.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ExampleResolver } from '../graphql/resolvers/example/example.resolver';
 import { PeopleResolver } from '../graphql/resolvers/people/people.resolver';
 import { DomainModule } from '../../domain/domain.module';
 import { StatisticsResolver } from '../graphql/resolvers/statistics/statistics.resolver';
 
 @Module({
   imports: [DomainModule],
-  providers: [ExampleResolver, PeopleResolver, StatisticsResolver],
+  providers: [PeopleResolver, StatisticsResolver],
 })
 export class GatewayModule {}
